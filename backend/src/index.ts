0aaa56f724ba2ef9eb87c92aa8228d09bd450336
Sebastian Itamari <sebaton66@gmail.com>
2026-07-21 20:35:29 -0400

    ci: introduce a simulation of error in the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,10 +18,16 @@ app.get("/", (req: Request, res: Response) => {
   res.send("¡El backend está funcionando!");
 });
 
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "ok" });
+});
+
 // Integrate routes
 app.use("/tasks", require("./routes/tasks"));
 app.use("/auth", require("./routes/auth"));
 app.use("/test", require("./routes/test"));
+
+throw new Error('fallo simulado');
 
 // Start the server
 app.listen(PORT, () => {
